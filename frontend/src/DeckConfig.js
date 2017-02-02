1e8b8d054f513b283c4f34906b7cc11273088bd9
DinoTrainer <tunell.john@gmail.com>
2017-02-02 11:55:24 +0100

    Added radio buttons together with tests

--- frontend/src/DeckConfig.js
+++ frontend/src/DeckConfig.js
@@ -2,6 +2,19 @@ import React, {Component} from 'react'
 import {Link} from "react-router";
 
 export default class DeckConfig extends Component {
+    constructor() {
+        super()
+        this.state = {
+            hideGenerate: true
+        }
+        this.onDeckTypeChange = this.onDeckTypeChange.bind(this)
+    }
+
+    onDeckTypeChange(e) {
+        console.log(e.target.value)
+        console.log("hide generate: " + (e.target.value === 'existingDeck'))
+        this.setState({showGenerate: e.target.value === 'existingDeck'})
+    }
 
     render() {
         return (
@@ -11,6 +24,10 @@ export default class DeckConfig extends Component {
                 }
 
                 <h1>Deck configuration</h1>
+                <form onChange={this.onDeckTypeChange}>
+                    <input type="radio" name="selectType" value="existingDeck" />Choose an existing deck of flashcards<br />
+                    <input type="radio" name="selectType" value="generateDeck" />Generate a deck
+                </form>
                 <select onChange={event => this.props.onDeckChange(event.target.value)}>
                     {
                         this.props.decks.map((deck, index) => (
@@ -32,3 +49,4 @@ export default class DeckConfig extends Component {
         )
     }
 }
+

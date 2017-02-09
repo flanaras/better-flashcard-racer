80488a188fdc44bdd289228541e53546c548b6a4
ilona-asa <asailona@gmail.com>
2017-02-09 21:58:50 +0100

    implement resources for endpoint \decks\{id} and update yaml file

--- backend/src/main/java/se/uu/it/bfcr/inflector/springboot/controllers/DeckController.java
+++ backend/src/main/java/se/uu/it/bfcr/inflector/springboot/controllers/DeckController.java
@@ -39,54 +39,59 @@ import java.util.List;
 
 @Component
 public class DeckController {
+	private Connection con = null;
+	private String selectCards = "SELECT card.id as card_id, problem, solution FROM deck_card_dep, card where deck_card_dep.card_id=card.id AND deck_card_dep.deck_id = ?";
+	private List<Deck> decksList = new ArrayList<Deck>();
 
-    public ResponseContext getDecks(RequestContext requestContext) throws SQLException {
-    	//put some query here
-        Connection con = null;
-        PreparedStatement preparedStm1 = null, preparedStm2 = null;
-        List<Deck> decks = new ArrayList<Deck>();
+	public List<Deck> populateDeck(ResultSet rs) throws SQLException{
+		PreparedStatement preparedStm2 = null;
+		List<Deck> decks = new ArrayList<Deck>();
+		while (rs.next()) {
+			Deck deck = new Deck();
+			deck.setUserId(rs.getInt("user_id"));
+			deck.setName(rs.getString("name"));
+			deck.setCreated(rs.getString("created"));
+			deck.setChanged(rs.getString("changed"));
+			deck.setDescription(rs.getString("description"));
+			deck.setId(rs.getInt("id"));
+			if(rs.getInt("user_id") == 0){
+				deck.setIsPrivate(false);
+			}else {
+				deck.setIsPrivate(true);
+			}
+			deck.setNumProblems(rs.getInt("num_problem"));
+			deck.setUserName(rs.getString("username"));
+
+			preparedStm2 = con.prepareStatement(selectCards);
+			preparedStm2.setInt(1, rs.getInt("id"));
+			ResultSet rsCards = preparedStm2.executeQuery();
+
+			List<Flashcard> cards = new ArrayList<Flashcard>();
+			while (rsCards.next()) {
+				Flashcard card = new Flashcard();
+				card.setId(rsCards.getInt("card_id"));
+				card.setProblem(rsCards.getString("problem"));
+				card.setAnswer(rsCards.getString("solution"));
+				cards.add(card);
+			}
+
+			deck.setFlashcard(cards);
+
+			decks.add(deck);
+		}
+		return decks;
+	}
+
+	public ResponseContext getDecks(RequestContext requestContext) throws SQLException {
+        PreparedStatement preparedStm1 = null;
         
         try {
             con = DBConnect.connect();
-            String selectDecks = "SELECT COUNT(*) as num_problem, decks.id as id, decks.name, decks.description, decks.difficulty, decks.private, decks.created, decks.changed, users.id as user_id, users.username FROM `decks`, users, deck_card_dep where decks.created_by = users.id AND decks.id=deck_card_dep.deck_id GROUP BY deck_card_dep.deck_id";
-            String selectCards = "SELECT card.id as card_id, problem, solution FROM deck_card_dep, card where deck_card_dep.card_id=card.id AND deck_card_dep.deck_id = ?";
-            
-            preparedStm1 = con.prepareStatement(selectDecks);
-            ResultSet rs = preparedStm1.executeQuery();
+			String selectDecks = "SELECT COUNT(*) as num_problem, decks.id as id, decks.name, decks.description, decks.difficulty, decks.private, decks.created, decks.changed, users.id as user_id, users.username FROM `decks`, users, deck_card_dep where decks.created_by = users.id AND decks.id=deck_card_dep.deck_id GROUP BY deck_card_dep.deck_id";
+			preparedStm1 = con.prepareStatement(selectDecks);
 
-			while (rs.next()) {
-				Deck deck = new Deck();
-				deck.setUserId(rs.getInt("user_id"));
-		        deck.setName(rs.getString("name"));
-		        deck.setCreated(rs.getString("created"));
-		        deck.setChanged(rs.getString("changed"));
-		        deck.setDescription(rs.getString("description"));
-		        deck.setId(rs.getInt("id"));
-		        if(rs.getInt("user_id") == 0){
-		        	deck.setIsPrivate(false);
-		        }else {
-		        	deck.setIsPrivate(true);
-		        }
-		        deck.setNumProblems(rs.getInt("num_problem"));
-		        deck.setUserName(rs.getString("username"));
-		        
-				preparedStm2 = con.prepareStatement(selectCards);
-				preparedStm2.setInt(1, rs.getInt("id"));
-				ResultSet rsCards = preparedStm2.executeQuery();
-				
-				List<Flashcard> cards = new ArrayList<Flashcard>();
-				while (rsCards.next()) {
-					Flashcard card = new Flashcard();
-					card.setId(rsCards.getInt("card_id"));
-					card.setProblem(rsCards.getString("problem"));
-					card.setAnswer(rsCards.getString("solution"));
-					cards.add(card);
-				}
-				
-				deck.setFlashcard(cards);
-				
-				decks.add(deck);
-			}
+            ResultSet rs = preparedStm1.executeQuery();
+			decksList = populateDeck(rs);
             
         } catch (SQLException ex) {
             Logger.getLogger(DeckController.class.getName()).log(Level.SEVERE,
@@ -101,8 +106,36 @@ public class DeckController {
         }
 
         return new ResponseContext().status(Status.OK)
-                                    .entity(decks);
+                                    .entity(decksList);
     }
 
+	public ResponseContext getDecksById(RequestContext requestContext, Long id) throws SQLException {
+		PreparedStatement preparedStm1 = null;
+
+		try {
+			con = DBConnect.connect();
+			String selectDecks = "SELECT COUNT(*) as num_problem, decks.id as id, decks.name, decks.description, decks.difficulty, decks.private, decks.created, decks.changed, users.id as user_id, users.username FROM `decks`, users, deck_card_dep where decks.created_by = users.id AND decks.id=deck_card_dep.deck_id AND decks.id = ? GROUP BY deck_card_dep.deck_id";
+
+			preparedStm1 = con.prepareStatement(selectDecks);
+			preparedStm1.setInt(1, id.intValue());
+			ResultSet rs = preparedStm1.executeQuery();
+
+			decksList = populateDeck(rs);
+
+		} catch (SQLException ex) {
+			Logger.getLogger(DeckController.class.getName()).log(Level.SEVERE,
+					null, ex);
+		} finally {
+			try {
+				con.close();
+			} catch (SQLException ex) {
+				Logger.getLogger(DeckController.class.getName()).log(
+						Level.SEVERE, null, ex);
+			}
+		}
+
+		return new ResponseContext().status(Status.OK)
+				.entity(decksList);
+	}
     
 }

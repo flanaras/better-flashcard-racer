c171c413f40c150d09cf53db830efdf55896738b
Bartok <bartok00@hotmail.com>
2017-02-10 13:45:48 +0100

    Added the DBConnect to this branch for use in the UserController class and some minor set ups to facilitate furhter implementation

--- backend/src/main/java/se/uu/it/bfcr/inflector/springboot/controllers/DBConnect.java
+++ backend/src/main/java/se/uu/it/bfcr/inflector/springboot/controllers/DBConnect.java
@@ -22,13 +22,7 @@ public class DBConnect {
         }
 
         try {
-<<<<<<< 99db36fb97f69a7ff4ea0cb50b5148307a5e196d
             conn = DriverManager.getConnection(url, username, password);
-=======
-//            conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/" + username + "&" + password);
-        	conn = DriverManager.getConnection(url, username, password);
-
->>>>>>> fixing database connection, better-flashcard-racer/lib/mysql-connector-java-6.0.5.jar should be added into project, add mysql dependency in gradle
         } catch (SQLException ex) {
             System.out.println("SQLException: " + ex.getMessage());
             System.out.println("SQLState: " + ex.getSQLState());

f32032bc3c5024dcdc72fc1da71494562e9d20dd
Bartok <bartok00@hotmail.com>
2017-02-23 18:20:19 +0100

    Set up of the implementation of websocket and gameroom

--- backend/src/main/java/se/uu/it/bfcr/inflector/springboot/controllers/WebSocketEndpoint.java
+++ backend/src/main/java/se/uu/it/bfcr/inflector/springboot/controllers/WebSocketEndpoint.java
@@ -13,6 +13,7 @@ import se.uu.it.bfcr.inflector.springboot.models.GameRoom;
 /**
  * Created by Bartok on 2/21/17.
  */
+@ServerEndpoint("/room")
 public class WebSocketEndpoint {
 
     @OnOpen
@@ -29,7 +30,28 @@ public class WebSocketEndpoint {
     public void onMessage(String message, Session session){
         System.out.println("Message from " + session.getId() + ": " + message);
         try {
-            session.getBasicRemote().sendText(message);
+            /*
+            * Temporary notes:
+            *
+            * The different messages interpreted by the onMessage mehtod:
+            * "CR:room-id:creator" -> Create a game room.
+            * "JR:room-id" -> join a room.
+            * "LR:" -> leave a room
+            */
+            switch (message.substring(0,2)) {
+                case "CR:":
+                    //Create room logic...
+                    break;
+                case "JR:":
+                    //Join room logic...
+                    break;
+                case "LR:":
+                    //Leave room logic...
+                    break;
+                default:
+                    System.out.println("Unknown webSocket message");
+                    throw new IOException();
+            }
         } catch (IOException ex) {
             ex.printStackTrace();
         }

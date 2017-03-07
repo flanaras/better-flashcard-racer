b36f466f7f5c69e686a7f41c0d344674ff58fc97
Bartok <bartok00@hotmail.com>
2017-03-07 16:44:56 +0100

    Slight update to classes and methods in roomserver

--- roomserver/src/roomserver.py
+++ roomserver/src/roomserver.py
@@ -18,12 +18,13 @@ class User:
         self.sid = sid
 
 class Room:
-    def __init__(self, id, roomName, hostId, hostName):
+    def __init__(self, id, roomName, hostId, hostName, hostsid):
         self.id = id
         self.roomName = roomName
         self.hostId = hostId
         self.hostName = hostName
         self.players = []
+        self.hostsid = hostsid
 
 sio = socketio.AsyncServer()
 app = web.Application()
@@ -40,36 +41,35 @@ def connect(sid, environ):
 @sio.on('join_lobby', namespace='/lobby')
 async def join_lobby(sid, data):
     print("message ", data)
-
-    new_user = User(data.get('id'), data.get('username'), sid)
-
-    #print (new_user.id)
-    #print (new_user.username)
-    #print (new_user.sid)
-
-    Lobby.append(new_user)
+    Lobby.append(User(data.get('id'), data.get('username'), sid))
+    print('Lobby:')
+    for user in Lobby:
+        print(' ',user.username)
+        #Does not actually emit dta to clients yet:
     await sio.emit('updatelobby',namespace='/lobby')
+        #
 
 @sio.on('updatelobby',namespace='/lobby')
 def updatelobby(sid, data):
-    jsonstring = '{'
+    print('Users in lobby: ')
     for user in Lobby:
-        uname = user.username
         id = str(user.id)
-        jsonstring = jsonstring + '"id": ' + id
-        jsonstring = jsonstring + ', "username": "' + uname + '"'
-        jsonstring = jsonstring + ', "auth_level": ""'
-    jsonstring = jsonstring + '}'
-    print(jsonstring)
-    jsondata = json.dumps(jsonstring)
+        print('id: ',id)
+        print(' username: ',user.username)
+        #Does not actually provide a proper JSON object to client
+    jsondata = json.dumps('')
     return jsondata
+        #
 
 @sio.on('disconnect', namespace='/lobby')
 def disconnect(sid):
-    print('disconnected from lobby service: ', sid)
+    print('disconnected from lobby service: ', sid, 'name: ')
     for user in Lobby:
         if user.sid == sid:
             Lobby.remove(user)
+    print('Lobby:')
+    for user in Lobby:
+        print(' ',user.username)
 
 #Room responses:
 
@@ -77,8 +77,14 @@ def disconnect(sid):
 def disconnect(sid):
     print('disconnected from room service: ', sid)
 
-@sio.on('roomact', namespace='/room')
-async def roommessage(sid, data):
+#Needs to be implemented:
+@sio.on('create_room', namespace='/room')
+async def create_room(sid, data):
+    print("message ", data)
+
+#Needs to be implemented:
+@sio.on('leave_room', namespace='/room')
+async def leave_room(sid, data):
     print("message ", data)
 
 @sio.on('connect', namespace='/room')
